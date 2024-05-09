c8fd255465b573a04ef4ac259954d291fa24d134
Jose Olivarria <145067078+jolivarria1212@users.noreply.github.com>
2024-05-09 16:55:07 -0700

    Done

--- OlivarriaP5Unit5/Assets/Course Library/Scripts/GameManager.cs
+++ OlivarriaP5Unit5/Assets/Course Library/Scripts/GameManager.cs
@@ -11,22 +11,45 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoretext;
     public TextMeshProUGUI gameOverText;
     public Button restartButton;
+    public GameObject titleScreen;
+    public TextMeshProUGUI livesText;
+    public GameObject pauseScreen;
     public bool isGameActive;
+    private bool paused;
     private int score;
+    private int lives;
     private float spawnRate = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
+      
         
+    }
+
+    void ChangePaused()
+    {
+        if (!paused)
+        {
+            paused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            paused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePaused();
+        }
         
     }
     
@@ -46,6 +69,16 @@ public class GameManager : MonoBehaviour
         scoretext.text = "Score: " + score;
     }
 
+    public void UpdateLives(int livesToChange)
+    {
+        lives += livesToChange;
+        livesText.text = "Lives: " + lives;
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+    }
+
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
@@ -56,5 +89,16 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        spawnRate = spawnRate / difficulty;
+        UpdateScore(0);
+        UpdateLives(3);
+        titleScreen.gameObject.SetActive(false);
     }
 }
